3afb557780fdcd6b1ea4cc4a1b4d23709004dd87
SudevOP1 <sudevdahitule06@gmail.com>
2025-11-16 02:07:07 +0530

    drawing quads added

--- src/main/java/engine/graph/Mesh.java
+++ src/main/java/engine/graph/Mesh.java
@@ -4,6 +4,7 @@ import org.lwjgl.opengl.GL30;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.*;
 
 import static org.lwjgl.opengl.GL30.*;
@@ -14,8 +15,8 @@ public class Mesh {
     private int vaoId; // VAO = Vertex Array Object
     private List<Integer> vboIdList; // VBO = Vertex Buffer Object
 
-    public Mesh(float[] positions, int numVertices) {
-        this.numVertices = numVertices;
+    public Mesh(float[] positions, float colors[], int[] indices) {
+        numVertices = indices.length;
         vboIdList = new ArrayList<>();
 
         vaoId = glGenVertexArrays();
@@ -24,17 +25,39 @@ public class Mesh {
         // positions VBO
         int vboId = glGenBuffers();
         vboIdList.add(vboId);
-        FloatBuffer positionsBuffer = MemoryUtil.memCallocFloat(positions.length);
-        positionsBuffer.put(0, positions);
+        FloatBuffer positionsBuffer = MemoryUtil.memAllocFloat(positions.length);
+        positionsBuffer.put(positions).flip();
         glBindBuffer(GL_ARRAY_BUFFER, vboId);
         glBufferData(GL_ARRAY_BUFFER, positionsBuffer, GL_STATIC_DRAW);
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
 
+        // colors VBO
+        vboId = glGenBuffers();
+        vboIdList.add(vboId);
+        FloatBuffer colorsBuffer = MemoryUtil.memAllocFloat(colors.length);
+        colorsBuffer.put(colors).flip();
+        glBindBuffer(GL_ARRAY_BUFFER, vboId);
+        glBufferData(GL_ARRAY_BUFFER, colorsBuffer, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
+
+        // index VBO
+        vboId = glGenBuffers();
+        vboIdList.add(vboId);
+        IntBuffer indicesBuffer = MemoryUtil.memAllocInt(indices.length);
+        indicesBuffer.put(indices).flip();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+        
+        // unbind VBOs (VAO keeps the element buffer binding)
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
 
+        // free the buffers
         MemoryUtil.memFree(positionsBuffer);
+        MemoryUtil.memFree(colorsBuffer);
+        MemoryUtil.memFree(indicesBuffer);
     }
 
     public void cleanup() {
